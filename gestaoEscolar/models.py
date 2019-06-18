eb9a03e5dc0e1c3802ffdb51841ff1992a8ed7e1
Bruno Oliveira <brunooliveiraeoliveira@outlook.com>
2019-06-18 19:54:07 -0300

    Atualizando os campos e os relacionamentos entre as tabelas ja criadas

--- gestaoEscolar/models.py
+++ gestaoEscolar/models.py
@@ -25,9 +25,8 @@ class Escola(models.Model):
     )
     Tipo_Escola = models.CharField(max_length=20, choices=TIPOS, default=PUBLICA)
     #FKs
-    Diretor = ''
-    Endereco = ''
-
+    Diretor = models.OneToOneField('Gestor', on_delete=models.PROTECT,blank=True, null=True)
+    Endereco = models.OneToOneField('Endereco', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.Nome
@@ -36,8 +35,8 @@ class Escola(models.Model):
 class Telefone(models.Model):
     Numero = models.CharField(max_length=20)
     #FK
-    Pessoa = '' #OPCIONAL CASO FOR DE UMA ESCOLA
-    Escola = models.ForeignKey('Escola', on_delete=models.PROTECT)
+    Pessoa = models.ForeignKey('Pessoa', on_delete=models.PROTECT, blank=True, null=True)
+    Escola = models.ForeignKey('Escola', on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.Numero
@@ -57,15 +56,14 @@ class Endereco(models.Model):
         (RURAL,'Rural')
     )
     Zona = models.CharField(max_length=15, choices=TIPOS_ZONAS, default=URBANA)
-    #FK
-    Pessoa = 'OPCIONAL CASO FOR DE UMA ESCOLA'
-    Escola = models.ForeignKey('Escola', on_delete=models.PROTECT)
+    #Controlar para quem é obrigatório 
+    Pessoa = models.ForeignKey('Pessoa', on_delete=models.PROTECT, blank=True, null=True)
     
     def __str__(self):
         return self.Rua+' N°'+ str(self.Numero)
 
 
-class Pessoa(models.Model):
+class PessoaAbstract(models.Model):
     Nome = models.CharField(max_length=50)
     MASCULINO = 'M'
     FEMININO = 'F'
@@ -79,12 +77,18 @@ class Pessoa(models.Model):
     Rg = models.CharField(max_length=9, blank=True, null=True)
     Nome_Usuario = 'models.CharField(max_length=40, unique=True)'
     Email = 'models.EmailField(max_length=254)'
+    #Controlar para quem é obrigatório 
+    Escola = models.ForeignKey('Escola', on_delete=models.PROTECT, blank=True, null=True)
 
     class Meta:
         abstract = True
     
     def __str__(self):
         return self.Nome
+
+
+class Pessoa(PessoaAbstract):
+    pass
     
 
 class Gestor(Pessoa):
@@ -109,4 +113,73 @@ class Professor(Pessoa):
 
 
 class Aluno(Pessoa):
-    pass
+    Ra = models.CharField(max_length=20,blank=True, null=True, unique=True)
+    Cidade_Nascimento = models.CharField(max_length=40)
+    Estado_Nascimento = models.CharField(max_length=40)
+    Nacionalidade = models.CharField(max_length=20)
+    SIM = 'S'
+    NAO = 'N'
+    SIM_NAO = (
+        (SIM, 'Sim'),
+        (NAO, 'Não')
+    )
+    Irmao_Gemeo = models.CharField(max_length=10, choices=SIM_NAO, default=SIM)
+    Nome_Pai = models.CharField(max_length=40)
+    Nome_Mae = models.CharField(max_length=40)
+    Necessidade_Educacional_Especial = models.CharField(max_length=10, choices=SIM_NAO, default=SIM)
+    Superdotacao = models.CharField(max_length=10, choices=SIM_NAO, default=SIM)
+    MULTIPLA = 'multipla'
+    CEGUEIRA = 'cegueira'
+    BAIXA_VISAO = 'baixa_visao'
+    SURDEZ_SEVERA_OU_PROFUNDA = 'surdez_severa_ou_profunda'  
+    SURDEZ_SEVERA_OU_MODERADA = 'surdez_severa_ou_moderada' 
+    SURDOCEGUEIRA = 'surdocegueira'
+    FISICA_PARALISIA_CEREBRAL = 'fisica_paralisia_cerebral'
+    FISICA_CADEIRANTE = 'fisica_cadeirante'
+    FISICA_OUTROS = 'fisica_outros'
+    SINDROME_DE_DOWN = 'sindrome_de_down'
+    INTELECTUAL = 'intelectual'
+    NAO_POSSUI = 'nao_possui'
+    TIPOS_DEFICIENCIA = (
+        (NAO_POSSUI, 'Não Possui'),
+        (MULTIPLA, 'Múltipla'),
+        (CEGUEIRA, 'Cegueira'),
+        (BAIXA_VISAO, 'Baixa Visão'),
+        (SURDEZ_SEVERA_OU_PROFUNDA, 'Surdez Severa ou Profunda'),
+        (SURDEZ_SEVERA_OU_MODERADA, 'Surdez Leve ou Moderada'),
+        (SURDOCEGUEIRA, 'Surdocegueira'),
+        (FISICA_PARALISIA_CEREBRAL, 'Física – Paralisia Cerebral'),
+        (FISICA_CADEIRANTE, 'Física – Cadeirante'),
+        (FISICA_OUTROS, 'Física – Outros'),
+        (SINDROME_DE_DOWN, 'Síndrome de Down'),
+        (INTELECTUAL, 'Intelectual')
+    )
+    Deficiencia = models.CharField(max_length=40, choices=TIPOS_DEFICIENCIA, default=NAO_POSSUI)
+    AUTISTA_INFANTIL = 'autista_infantil'
+    SINDROME_DE_ASPERGER = 'sindrome_de_asperger'
+    SINDROME_DE_RETT = 'sindrome_de_rett'
+    TRANSTORNO_DESINTEGRATIVO_DE_INFANCIA = 'transtorno_desintegrativo_de_infancia'
+    TIPOS_TRANSTORNO = (
+        (NAO_POSSUI, 'Não Possui'),
+        (AUTISTA_INFANTIL, 'Autista Infantil'),
+        (SINDROME_DE_ASPERGER, 'Síndrome de Asperger'),
+        (SINDROME_DE_RETT, 'Síndrome de Rett'),
+        (TRANSTORNO_DESINTEGRATIVO_DE_INFANCIA, 'Transtorno Desintegrativo de Infância')
+    )
+    Transtorno_Global_do_Desenvolvimento = models.CharField(
+        max_length=40, 
+        choices=TIPOS_TRANSTORNO, 
+        default=NAO_POSSUI
+    )
+    Bolsa_Familia = models.CharField(max_length=10, choices=SIM_NAO, default=SIM)
+    
+    
+    
+    
+    
+     
+    
+    
+    
+
+    
